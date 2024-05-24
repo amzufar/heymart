6424b9c812afa9896bcfd5bd106b306f77c08292
Ammar Zufar <amzufar@gmail.com>
2024-05-24 08:49:45 +0700

    Initial commit

---
+++ src/main/java/heymart/heymart/HeymartApplication.java
@@ -0,0 +1,13 @@
+package heymart.heymart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeymartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeymartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heymart/heymart/HeymartApplicationTests.java
@@ -0,0 +1,13 @@
+package heymart.heymart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeymartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
